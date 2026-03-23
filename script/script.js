1d88afeee516439e74041d05f5c6ee0a4472e3d1
“Nilde <nilde.benedetti@gmail.com>
2026-03-23 16:20:15 +0100

    rimuovo tutti i console.log aggiunti per check, di modo da lasciare lésercizio pulito

--- script/script.js
+++ script/script.js
@@ -7,14 +7,12 @@
 // accetto di proposito anche valori con la virgola, non si sa mai che becchiamo qualche precisino
 
 const trattaKm = parseFloat(prompt('quanti chilometri devi percorrere (inserire solo valore numerico)?', '10'));
-console.log(trattaKm);
 
 // Prompt2: quanti anni hai?
 //  prendiamo questo inpunt e trasformiamolo nella variabile etaAnni
 // e converti in Integer (calcoliamo sui compleanni festeggiati) 
 
 const etaAnni = parseInt(prompt('quanti anni hai(inserire solo valore numerico', '18'));
-console.log(etaAnni);
 
 
 // dichiariamo e assegnamo una variabile per il prezzo di viaggio al km
@@ -24,7 +22,6 @@ const prezzoKm = 0.21;
 // creiamo ora una variabile per il prezzo al variare della tratta
 
 const prezzoTratta = prezzoKm * trattaKm;
-console.log(prezzoTratta);
 
 // sconto: da applicare SE si rientra nella fascia eta'
 
@@ -42,7 +39,6 @@ else {
     sconto = 0;
 }
 
-console.log(sconto);
 
 // prezzo finale = prezzoTratta - sconto
 
