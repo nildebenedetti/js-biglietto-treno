e8b7662dca416412897a5650fd90f60934773558
“Nilde <nilde.benedetti@gmail.com>
2026-03-23 15:39:54 +0100

    rendiamo la tratta una variabile const per ciascun ciclo di programma

--- script/script.js
+++ script/script.js
@@ -2,6 +2,11 @@
 //  prendiamo questo imput e trasformiamolo nella variabile (let) trattaKm
 // e converti in float (ci servono tassativamente le virgole) 
 
-let trattaKm = parseFloat(prompt('quanti chilometri devi percorrere (inserire solo valore numerico)?', '10'));
+const trattaKm = parseFloat(prompt('quanti chilometri devi percorrere (inserire solo valore numerico)?', '10'));
 console.log(trattaKm);
+
+// Prompt2: quanti anni hai?
+//  prendiamo questo inpunt e trasformiamolo nella variabile (let) etaAnni
+// e converti in float (ci servono tassativamente le virgole) 
+
 
