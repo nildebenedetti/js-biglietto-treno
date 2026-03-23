5367936dc96c5ab51cdfc1b26cf71cd9954bd843
“Nilde <nilde.benedetti@gmail.com>
2026-03-23 15:38:41 +0100

    add prompt per ottenere chilometraggio da utente

--- script/script.js
+++ script/script.js
@@ -1,1 +1,7 @@
-console.log('hello world'); 
+// Prompt1: quanti chilometri devi percorrere?
+//  prendiamo questo imput e trasformiamolo nella variabile (let) trattaKm
+// e converti in float (ci servono tassativamente le virgole) 
+
+let trattaKm = parseFloat(prompt('quanti chilometri devi percorrere (inserire solo valore numerico)?', '10'));
+console.log(trattaKm);
+
